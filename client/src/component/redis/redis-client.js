906e53fc3fb3417d07b7fe8d4e6e699557d736e5
Imran Sahil <imdshizzle@gmail.com>
2024-07-28 15:28:58 -0400

    added old code back

--- client/src/component/redis/redis-client.js
+++ client/src/component/redis/redis-client.js
@@ -3,3 +3,33 @@ import { createClient } from "redis";
 export const redis = createClient({
   url: "redis://localhost:6379", // Update this URL to your Redis server URL if different
 });
+
+class RedisClinet {
+  constructor() {
+    this.connect();
+  }
+
+  async connect() {
+    await redis.connect();
+    redis.on("error", (err) => console.log("Redis Client Error", err));
+  }
+
+  // Function to set a key-value pair in Redis
+  async setKeyValue(key, value) {
+    await redis.set(key, value);
+    console.log(`Set ${key} to ${value}`);
+  }
+
+  // Function to get a value by key from Redis
+  async getValueByKey(key) {
+    const value = await redis.get(key);
+    console.log(`Value for ${key}: ${value}`);
+    return value;
+  }
+
+  async disconnect() {
+    await redis.quit();
+  }
+}
+
+export default RedisClinet;
