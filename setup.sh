433ce0ecb0b23ac90e752c6a06ab523f588edbb1
Imran Sahil <imdshizzle@gmail.com>
2024-10-23 10:10:22 -0400

    setup scripts updates

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
-echo "setting up the server"
+echo "Setting up the server..."
 cd server
-python -m venv venv
-# source venv/bin/activate
+# python3 -m venv .venv
+# source .venv/bin/activate
+python -m venv .venv
 source venv/Scripts/Activate
 pip install -r requirements.txt
-echo "server set up finished"
+echo "Server set up finished!"
 
 cd ..
 
-echo "setting up the client"
+echo "Setting up the client..."
 cd client
 npm install
-echo "client set up finished"
+echo "Client set up finished!"
